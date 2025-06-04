13db5af2f3f3d5c0b8c7269c8137f4624263422c
Atchutha Rao T A <atchutha57@gmail.com>
2025-06-04 09:51:27 +0530

    Remove unused index.ts file and its associated server setup code

--- index.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { serve } from "@hono/node-server";
-import { allRoutes } from "./src/routes/routes.ts";
-import { Hono } from "hono";
-
-const app = new Hono();
-
-app.route("/", allRoutes);
-serve(app);
